3e67ccc9637219b69ab403df364d7450935cc0bd
Virlence5 <dongmovirlence@gmail.com>
2025-01-17 15:54:00 +0100

    Créations des modèles et intégration dans le fichier admin de l'application Produit.

--- Produits/admin.py
+++ Produits/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Product, Customer, Sale, Bill
 
-# Register your models here.
+
+class Product_Manager(admin.ModelAdmin):
+    list_display=['name','price','quantity', 'add_date']
+
+
+class Customer_Manager(admin.ModelAdmin):
+    list_display=['name']
+
+
+class Sale_Manager(admin.ModelAdmin):
+    list_display=['customer','quantity','total_amount','sale_date']
+
+
+class Bill_Manager(admin.ModelAdmin):
+    list_display=['customer','price','quantity', 'add_date']
+
+
+
+admin.site.register(Product, Product_Manager)
+admin.site.register(Customer, Customer_Manager)
+admin.site.register(Sale, Sale_Manager)
+admin.site.register(Bill, Bill_Manager)
--- Produits/models.py
+++ Produits/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model) :
+    name = models.CharField(max_length=250)
+
+
+class Product(models.Model) :
+    name = models.CharField(max_length=250)
+    price = models.IntegerField()
+    quantity = models.PositiveIntegerField(default=0)
+    description = models.TextField()
+    add_date = models.DateTimeField(auto_now_add=True)
+    expiration_date = models.DateField()
+    image = models.ImageField(null=True, blank=True, upload_to='media/')
+
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta :
+        ordering = ['-add_date']
+
+
+    def quantity_status(self) :
+        if self.quantity == 0 :
+            return 'red'
+        elif self.quantity < 10 or self.quantity == 10 :
+            return 'orange'
+        else :
+            return 'green'
+
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=250)
+
+
+class Sale(models.Model) :
+    sale_date = models.DateTimeField(auto_now_add=True)
+    quantity = models.PositiveIntegerField(default=0)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.products
+
+
+class Bill(models.Model) :
+    quantity = models.PositiveIntegerField(default=0)
+    sale_date = models.DateTimeField(auto_now_add=False)
+    total_amount = models.ForeignKey(Sale, on_delete=models.CASCADE)
+
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Le reçu de {self.customer.name}"
+
+    
