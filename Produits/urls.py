c56c3f352779dc8ddb22f7112a40b3fed853f52b
Virlence5 <dongmovirlence@gmail.com>
2025-01-20 05:51:37 +0100

    Implémentation du formulaire d'ajout d'un produit.

--- Produits/urls.py
+++ Produits/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from Produits import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.home, name='home'),
-]
+    path('add_product', views.add_product, name='add_product'),
+    
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Produits/views.py
+++ Produits/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, redirect
+from .models import *
 
 def home(request) :
     products = Product.objects.all()
     return render(request,'home.html',{'products':products})
+
+
+def add_product(request):
+    if(request.method=="POST"):
+        name=request.POST['name']
+        quantity=request.POST['quantity']
+        price=request.POST['price']
+        description=request.POST['description']
+        expiration_date=request.POST['expiration_date']
+        image=request.FILES['image']
+        category=Category.objects.get(pk=request.POST['category'])
+
+        save_data=Product(name=name,quantity=quantity,price=price,description=description,expiration_date=expiration_date, image=image, category=category)
+        save_data.save()
+        return redirect('home')
+    else:
+        category=Category.objects.all()
+    return render(request,'form_add_product.html',{'category':category})
+
 
