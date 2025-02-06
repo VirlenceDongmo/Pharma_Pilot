9c54251298fe25df49d52490fd0925367bb8908d
Virlence5 <dongmovirlence@gmail.com>
2025-02-06 17:10:05 +0100

    Ajout de la fonctionnalité de création de compte utilisateur.

--- Authentification_du_systeme/urls.py
+++ Authentification_du_systeme/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 
 urlpatterns = [
-    path('login/', login_view, name='login'),
-    # path('home', home, name='home'),
+    path('login', login_view, name='login'),
+    path('sign_up', sign_up_view, name='sign_up'),
     # path('add_products',Add_products.as_view() , name='add_products'),
     # path('update/<int:id>',update_product , name='update'),
     # path('delete/<int:id>',delete_product , name='delete'),
--- Authentification_du_systeme/views.py
+++ Authentification_du_systeme/views.py
@@ -1,6 +1,49 @@
 from django.contrib import messages
 from django.shortcuts import redirect, render
 from django.contrib.auth import login, authenticate
+import re
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+
+
+
+def sign_up_view(request):
+    if request.method=="POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password_confirm = request.POST.get('password_confirm')
+
+        if password != password_confirm:
+            messages.error(request, "passwords are not the same, please try again !")
+            return redirect('sign_up')
+
+        if len(password) < 8 or not re.search(r'[A-Za-z]', password) or not re.search(r'\d', password) or not re.search(r'[!@#$%^&*(),-_+=<>?/;"\'\`~:{}[\]|]', password):
+            messages.error(request,"The password must contains at least 8 characters, including letters, numbers and specials characters")
+            return render('sign_up')
+        
+        try :
+            validate_email(email)
+        except ValidationError:
+            messages.error(request, "Your email address is invalid, please try again.")
+            return render('sign_up')
+        
+        if User.objects.filter(username=username).exists():
+            messages.error=(request,"This username already exists on this app, please try again whith another one.")
+            return render('sign_up')
+        
+        if User.objects.filter(email=email).exists():
+            messages.error=(request,"This email address already exists on this app, please try again whith another one.")
+            return render('sign_up')
+        
+        User.objects.create_user(username=username, email=email, password=password)
+        messages.success(request, "Account created successfully ! Now you can login")
+
+        return redirect('login')
+    return render(request, 'sign_up.html')
+
+
 
 
 
